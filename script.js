6a005b741a559f33c61abc79f8fdfaef0e0fed55
pawelstasiewicz <pst357357@gmail.com>
2024-08-20 16:48:05 +0200

    First commit - Created game board for Snake

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas')
+
+canvas.style.backgroundColor = '#fff'
+canvas.style.border = '2px solid #000'
+
+let snake = [ {x: 150, y: 150}, {x: 140, y: 150}, {x: 130, y: 150}, {x: 120, y: 150}, {x: 110, y: 150}]
